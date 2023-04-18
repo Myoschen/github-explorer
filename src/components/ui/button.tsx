c4e1fad5396a4dc9c0cd1ffda193b2f1ca4548ea
Myos <myos.chen@gmail.com>
2023-04-19 00:43:26 +0800

    :recycle: use custom cn function instead of classnames

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,22 @@
 import {ComponentPropsWithoutRef} from 'react';
+import {cn} from '@/utils/classnames';
 
 interface Props extends ComponentPropsWithoutRef<'button'> {
   icon?: JSX.Element;
-  text: string;
+  text?: string;
 }
 
-function Button({icon, text, ...props}: Props) {
+function Button({icon, text, className, ...props}: Props) {
   return (
     <button
-      className="text-mauve12 inline-flex cursor-default items-center gap-x-1 rounded-md bg-whiteA11 px-3 py-1 font-paragraph text-sm shadow-sm hover:bg-whiteA12 focus-visible:outline focus-visible:outline-2 focus-visible:outline-gray6 dark:bg-whiteA5 dark:hover:bg-whiteA6 dark:focus-visible:outline-gray12 "
+      className={cn(
+        'text-mauve12 inline-flex cursor-default items-center gap-x-1 rounded-md bg-whiteA11 px-3 py-1 font-paragraph text-sm shadow-sm hover:bg-whiteA12 focus-visible:outline focus-visible:outline-2 focus-visible:outline-gray6 dark:bg-whiteA5 dark:hover:bg-whiteA6 dark:focus-visible:outline-gray12',
+        className
+      )}
       {...props}
     >
       {icon}
-      <span>{text}</span>
+      {text ? <span>{text}</span> : null}
     </button>
   );
 }
--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,5 +1,5 @@
 import {motion} from 'framer-motion';
-import cn from 'classnames';
+import {cn} from '@/utils/classnames';
 
 interface Props {
   text: string;
