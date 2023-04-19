2d7ef190c859a4333f044b3375d6a73e34157e15
Myos <myos.chen@gmail.com>
2023-04-19 15:24:41 +0800

    :lipstick: change bg and text color in dark mode

--- src/components/search/search-input.tsx
+++ src/components/search/search-input.tsx
@@ -12,7 +12,7 @@ function SearchInput({value, onChange, onReset}: Props) {
   return (
     <motion.div
       layoutId="search-input"
-      className="flex flex-1 rounded-md border bg-white text-gray2 shadow-sm focus-within:outline focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray6 dark:focus-within:outline-gray12"
+      className="flex flex-1 rounded-md  bg-white text-gray2 shadow-sm focus-within:outline focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray6 dark:bg-gray1 dark:text-white dark:focus-within:outline-gray12"
     >
       <label
         className="inline-flex items-center rounded-l-md px-2 "
@@ -21,7 +21,7 @@ function SearchInput({value, onChange, onReset}: Props) {
         <MagnifyingGlassIcon />
       </label>
       <input
-        className="-ml-px block w-full flex-1 rounded-r-md border-none pl-0 font-paragraph focus:outline-none focus:ring-0 focus-visible:outline-none sm:text-sm"
+        className="-ml-px block w-full flex-1 rounded-r-md border-none bg-inherit pl-0 font-paragraph text-inherit focus:outline-none focus:ring-0 focus-visible:outline-none sm:text-sm"
         id="search"
         type="text"
         value={value}
