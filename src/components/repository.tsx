a29f393b4df6fe944c4429a517164afdffd82529
Myos <myos.chen@gmail.com>
2024-01-01 23:40:24 +0800

    fix: truncate line and format stat count

--- src/components/repository.tsx
+++ src/components/repository.tsx
@@ -2,6 +2,7 @@ import { CircleDotIcon, GitForkIcon, StarIcon } from 'lucide-react'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { formatCount } from '@/lib/utils'
 
 interface RepositoryProps {
   // owner
@@ -29,14 +30,14 @@ export default function Repository({ ownerName, avatar, ownerUrl, name, descript
               <AvatarFallback>{ownerName?.charAt(0)}</AvatarFallback>
             </Avatar>
           </a>
-          <a href={repoUrl} target={'_blank'} rel={'noreferer'}>
-            <CardTitle className={'text-lg hover:underline'}>{name}</CardTitle>
+          <a className={'min-w-0'} title={name} href={repoUrl} target={'_blank'} rel={'noreferer'}>
+            <CardTitle className={'truncate text-lg hover:underline'}>{name}</CardTitle>
           </a>
         </div>
       </CardHeader>
       <CardContent>
         <div className={'flex flex-col gap-y-6'}>
-          <p>{description}</p>
+          <p className={'line-clamp-3 basis-[72px]'}>{description}</p>
           <div className={'flex items-center justify-between'}>
             <div className={'text-center'}>
               <span className={'flex items-center gap-x-1'}>
@@ -44,7 +45,7 @@ export default function Repository({ ownerName, avatar, ownerUrl, name, descript
                 {'Forks'}
               </span>
               <div className={'text-xl font-medium md:text-3xl'}>
-                {forks}
+                {forks > 1000 ? `${formatCount(forks)}k` : forks}
               </div>
             </div>
             <div className={'text-center'}>
@@ -53,7 +54,7 @@ export default function Repository({ ownerName, avatar, ownerUrl, name, descript
                 {'Issues'}
               </span>
               <div className={'text-xl font-medium md:text-3xl'}>
-                {issues}
+                {issues > 1000 ? `${formatCount(issues)}k` : issues}
               </div>
             </div>
             <div className={'text-center'}>
@@ -62,7 +63,7 @@ export default function Repository({ ownerName, avatar, ownerUrl, name, descript
                 {'Stars'}
               </span>
               <div className={'text-xl font-medium md:text-3xl'}>
-                {stargazers}
+                {stargazers > 1000 ? `${formatCount(stargazers)}k` : stargazers}
               </div>
             </div>
           </div>
