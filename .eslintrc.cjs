9c43f5024e154e018cdb3a3b1fa9a7118c2e3e72
Myos <myos.chen@gmail.com>
2024-01-01 16:53:30 +0800

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,53 @@
+const stylistic = require('@stylistic/eslint-plugin')
+
+const customized = stylistic.configs.customize({
+  flat: false,
+  indent: 2,
+  jsx: true,
+  quotes: 'single',
+  semi: false,
+})
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:tailwindcss/recommended'],
+  plugins: ['react-refresh', '@stylistic', 'simple-import-sort', 'unused-imports'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    tailwindcss: {
+      callees: ['cn', 'cva'],
+    },
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+
+    ...customized.rules,
+    '@stylistic/jsx-curly-brace-presence': ['error', 'always'],
+
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^react', '^next', '@?\\w'],
+          ['^~/.*'],
+          ['^@/.*'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
   },
-  plugins: ['react', 'prettier'],
-  rules: {},
 };
